d2dbd728a63de54bbdc4d01f03efbc448f81cb9c
DerrickGHW <derrick@bolt.com>
2022-08-25 13:25:24 -0400

    fix release flow

--- .circleci/scripts/release-artifacts.sh
+++ .circleci/scripts/release-artifacts.sh
@@ -2,14 +2,4 @@
 GIT_TAG=$1
 
 go get github.com/tcnksm/ghr
-ghr -t "${GITHUBTOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -n "Version $GIT_TAG" -body "This release is version $GIT_TAG" -debug "$GIT_TAG" "../artifacts/$GIT_TAG-f21.zip"
-ghr -t "${GITHUBTOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -n "Version $GIT_TAG" -body "This release is version $GIT_TAG" -debug "$GIT_TAG" "../artifacts/$GIT_TAG-lucky.zip"
-ghr -t "${GITHUBTOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -n "Version $GIT_TAG" -body "This release is version $GIT_TAG" -debug "$GIT_TAG" "../artifacts/$GIT_TAG-brooks.zip"
-ghr -t "${GITHUBTOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -n "Version $GIT_TAG" -body "This release is version $GIT_TAG" -debug "$GIT_TAG" "../artifacts/$GIT_TAG-core.zip"
-
-# upload the diff files, if they exist
-for filename in ../artifacts/*.txt; do
-    if test -f "$filename"; then 
-        ghr -t "${GITHUBTOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -n "Version $GIT_TAG" -body "This release is version $GIT_TAG" -debug "$GIT_TAG" "${filename}"
-    fi
-done
+ghr -t "${GITHUBTOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -n "Version $GIT_TAG" -body "This release is version $GIT_TAG" -debug "$GIT_TAG" "../artifacts/$GIT_TAG.zip"
