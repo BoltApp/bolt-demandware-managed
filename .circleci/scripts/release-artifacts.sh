48ec274cd96f5951bbe07309382c196e90bfe251
DerrickGHW <derrick@bolt.com>
2022-08-25 14:49:39 -0400

    attempt fix img version

--- .circleci/scripts/release-artifacts.sh
+++ .circleci/scripts/release-artifacts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 GIT_TAG=$1
 
-go get github.com/tcnksm/ghr
+go install github.com/tcnksm/ghr@latest
 ghr -t "${GITHUBTOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -n "Version $GIT_TAG" -body "This release is version $GIT_TAG" -debug "$GIT_TAG" "../artifacts/$GIT_TAG.zip"
