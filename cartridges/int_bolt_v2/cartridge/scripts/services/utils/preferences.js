559752e2bcc19b4adf811796945086c0b8ab7b19
DerrickGHW <derrick@bolt.com>
2022-09-01 14:07:54 -0400

    local dev

--- cartridges/int_bolt_v2/cartridge/scripts/services/utils/preferences.js
+++ cartridges/int_bolt_v2/cartridge/scripts/services/utils/preferences.js
@@ -1,9 +1,8 @@
-"use strict";
+'use strict';
 
 /* API Includes */
 var Site = require('dw/system/Site');
 /* Script Includes */
-
 
 var LogUtils = require('~/cartridge/scripts/utils/boltLogUtils');
 
@@ -16,66 +15,66 @@ exports.BOLT_CARTRIDGE_VERSION = '21.4.21';
  */
 
 exports.getSitePreferences = function () {
-  var site = Site.getCurrent(); // TODO: make v2 of these preference values https://app.asana.com/0/1201059489220902/1201809981962235
+    var site = Site.getCurrent(); // TODO: make v2 of these preference values https://app.asana.com/0/1201059489220902/1201809981962235
 
-  var boltMultiPublishableKey = site.getCustomPreferenceValue('boltMultiPublishableKeyOCAPI') || '';
-  var blockedCharactersList = site.getCustomPreferenceValue('blockedCharactersList') || null;
+    var boltMultiPublishableKey = 'xucIhuC7kR07.FljG3LVceZat.4e91eb678c187f0820a3ce27fc87b684f0d2797bf3439d5b34da164ff4e27771';
+    var blockedCharactersList = site.getCustomPreferenceValue('blockedCharactersList') || null;
 
-  if (boltMultiPublishableKey === '') {
-    log.error('Error: Bolt publishable key configurations are missing in Business Manager.');
-    return {};
-  }
+    if (boltMultiPublishableKey === '') {
+        log.error('Error: Bolt publishable key configurations are missing in Business Manager.');
+        return {};
+    }
 
-  var boltCdnUrl = boltConnectURL();
-  return {
-    boltEnable: Site.getCurrent().getCustomPreferenceValue('boltEnable'),
-    boltEnableCartPage: Site.getCurrent().getCustomPreferenceValue('boltEnableCartPage') || false,
-    boltMerchantDivisionID: Site.getCurrent().getCustomPreferenceValue('boltMerchantDivisionID') || '',
-    boltCdnUrl: boltCdnUrl,
-    boltMultiPublishableKey: boltMultiPublishableKey,
-    blockedCharactersList: blockedCharactersList,
-    boltEnableSessionRecording: Site.getCurrent().getCustomPreferenceValue('boltEnableSessionRecording') || false
-  };
+    var boltCdnUrl = boltConnectURL();
+    return {
+        boltEnable: Site.getCurrent().getCustomPreferenceValue('boltEnable'),
+        boltEnableCartPage: Site.getCurrent().getCustomPreferenceValue('boltEnableCartPage') || false,
+        boltMerchantDivisionID: Site.getCurrent().getCustomPreferenceValue('boltMerchantDivisionID') || '',
+        boltCdnUrl: boltCdnUrl,
+        boltMultiPublishableKey: boltMultiPublishableKey,
+        blockedCharactersList: blockedCharactersList,
+        boltEnableSessionRecording: Site.getCurrent().getCustomPreferenceValue('boltEnableSessionRecording') || false
+    };
 };
 /**
  * Return API URL
  * @returns {string} API URL to load connect from
  */
 
-
 exports.getBoltApiServiceURL = function boltApiURL() {
-  var boltEnv = Site.getCurrent().getCustomPreferenceValue('boltEnvironmentOCAPI').valueOf();
+    return 'https://api.derrick.dev.bolt.me';
+    var boltEnv = Site.getCurrent().getCustomPreferenceValue('boltEnvironmentOCAPI').valueOf();
 
-  switch (boltEnv) {
-    case 'sandbox':
-      return 'https://api-sandbox.bolt.com';
+    switch (boltEnv) {
+        case 'sandbox':
+            return 'https://api-sandbox.bolt.com';
 
-    case 'staging':
-      return 'https://api-staging.bolt.com';
+        case 'staging':
+            return 'https://api-staging.bolt.com';
 
-    case 'production':
-    default:
-      return 'https://api.bolt.com';
-  }
+        case 'production':
+        default:
+            return 'https://api.bolt.com';
+    }
 };
 /**
  * Return CDN URL
  * @returns {string} CDN URL to load connect from
  */
 
-
 function boltConnectURL() {
-  var boltEnv = Site.getCurrent().getCustomPreferenceValue('boltEnvironmentOCAPI').valueOf();
+    return 'https://connect.derrick.dev.bolt.me';
+    var boltEnv = Site.getCurrent().getCustomPreferenceValue('boltEnvironmentOCAPI').valueOf();
 
-  switch (boltEnv) {
-    case 'sandbox':
-      return 'https://connect-sandbox.bolt.com';
+    switch (boltEnv) {
+        case 'sandbox':
+            return 'https://connect-sandbox.bolt.com';
 
-    case 'staging':
-      return 'https://connect-staging.bolt.com';
+        case 'staging':
+            return 'https://connect-staging.bolt.com';
 
-    case 'production':
-    default:
-      return 'https://connect.bolt.com';
-  }
+        case 'production':
+        default:
+            return 'https://connect.bolt.com';
+    }
 }
