24a67c5d2049be89397c71084ac4ca514f02d555
Ning W <phpdev2012@126.com>
2023-02-23 22:51:07 +0800

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,32 @@
 
 var path = require('path');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var jsFiles = require('sgmf-scripts').createJsPath();
-var scssFiles = require('sgmf-scripts').createScssPath();
+var { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+//find cartridge data
+var entryFilePath = '';
+var outputFile = '';
+process.argv.forEach((val, index) => {
+    if (val === 'entryFilePath' ) {
+        entryFilePath = process.argv[index + 1];
+        var pathLength = entryFilePath.split('/').length;
+        outputFile = (entryFilePath.split('/')[pathLength-1]).split('.')[0];
+
+        console.log('entryFilePath: ', entryFilePath);
+        console.log('pathLength=', pathLength);
+        console.log('outputFile:', outputFile);
+    }
+});
+var outputPath = 'cartridges/' + entryFilePath.split('/')[1] + '/cartridge/static/default/';
+console.log("outputPath: ", outputPath);
 
 module.exports = [{
     mode: 'production',
     name: 'js',
-    entry: jsFiles,
+    entry: path.resolve(__dirname, `./${entryFilePath}`),
     output: {
-        path: path.resolve('./cartridges/int_bolt_sfra/cartridge/static'),
-        filename: '[name].js'
+        path: path.resolve(__dirname, `./${outputPath}js`),
+        filename: `${outputFile}.js`
     },
     module: {
         rules: [
@@ -32,10 +48,10 @@ module.exports = [{
 {
     mode: 'production',
     name: 'scss',
-    entry: scssFiles,
+    entry: path.resolve(__dirname, `./${entryFilePath}`),
     output: {
-        path: path.resolve('./cartridges/int_bolt_sfra/cartridge/static'),
-        filename: '[name]_tmp.css'
+        path: path.resolve(__dirname, `./${outputPath}css`),
+        filename: `${outputFile}_tmp.tmp`
     },
     module: {
         rules: [{
@@ -70,6 +86,10 @@ module.exports = [{
         }]
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: '[name].css' })
+        new MiniCssExtractPlugin({ filename: `${outputFile}.css` }),
+        new CleanWebpackPlugin({
+            protectWebpackAssets: false,
+            cleanAfterEveryBuildPatterns: ['*.tmp']
+        })
     ]
 }];
