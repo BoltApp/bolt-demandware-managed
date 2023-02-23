9a2182a1f182540f2f0dfd9d3f95e8dac0c80091
Ning W <phpdev2012@126.com>
2023-02-23 15:16:03 +0800

    fix scss compile issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,75 @@
-require('shelljs/make');
+'use strict';
+
 var path = require('path');
-var ExtractTextPlugin = require('sgmf-scripts')['extract-text-webpack-plugin'];
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var jsFiles = require('sgmf-scripts').createJsPath();
 var scssFiles = require('sgmf-scripts').createScssPath();
 
-module.exports = [
-  {
-    devtool: 'eval-source-map',
+module.exports = [{
     mode: 'production',
     name: 'js',
     entry: jsFiles,
     output: {
-      path: path.resolve('./cartridges/int_bolt_sfra/cartridge/static'),
-      filename: '[name].js',
+        path: path.resolve('./cartridges/int_bolt_sfra/cartridge/static'),
+        filename: '[name].js'
     },
     module: {
-      rules: [
-        {
-          test: /bootstrap(.)*\.js$/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread'],
-              cacheDirectory: true,
-            },
-          },
-        },
-      ],
-    },
-  },
-  {
-    mode: 'none',
+        rules: [
+            {
+                test: /bootstrap(.)*\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread'],
+                        cacheDirectory: true
+                    }
+                }
+            }
+        ]
+    }
+},
+{
+    mode: 'production',
     name: 'scss',
     entry: scssFiles,
     output: {
-      path: path.resolve('./cartridges/int_bolt_sfra/cartridge/static'),
-      filename: '[name].css',
+        path: path.resolve('./cartridges/int_bolt_sfra/cartridge/static'),
+        filename: '[name]_tmp.css'
     },
     module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'style-loader',
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                url: false,
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [require('autoprefixer')],
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sassOptions: {
-                  includePaths: [path.resolve('node_modules')],
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        url: false
+                    }
                 },
-              },
-            },
-          ],
-        },
-      ],
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [
+                                ['autoprefixer', {}]
+                            ]
+                        }
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: [
+                            path.resolve('node_modules')
+                        ]
+                    }
+                }
+            ]
+        }]
     },
     plugins: [
-      new ExtractTextPlugin({
-        filename: '[name].css',
-      }),
-    ],
-  },
-];
+        new MiniCssExtractPlugin({ filename: '[name].css' })
+    ]
+}];
