51134452839a60425fec76957bd165a468f8a026
justinBolt1987 <jxie@bolt.com>
2023-03-06 13:24:47 -0500

    Add comment for ApplePay listener init logic.

--- cartridges/int_bolt_sfra/cartridge/client/default/js/boltModalConfigure.js
+++ cartridges/int_bolt_sfra/cartridge/client/default/js/boltModalConfigure.js
@@ -4,5 +4,6 @@ var bolt = require('./bolt');
 
 $(document).ready(function () {
     bolt.initBoltButton();
+    // ignore the following line if not using ApplePay
     bolt.addApplePayHandlerIfNeeded();
 });
