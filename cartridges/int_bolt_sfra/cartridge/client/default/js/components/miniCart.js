824d2f044ffe6cb5c1b2bc8943f0597622578f11
Ning W <phpdev2012@126.com>
2023-03-27 17:15:16 +0800

    update

--- cartridges/int_bolt_sfra/cartridge/client/default/js/components/miniCart.js
+++ cartridges/int_bolt_sfra/cartridge/client/default/js/components/miniCart.js
@@ -5,7 +5,12 @@ var base = require('base/components/miniCart');
 module.exports = function () {
     base();
 
+    var minicartTotal = '';
+
     $('.minicart .popover').on('DOMSubtreeModified', function () {
         console.log($(this).children().length);
+        if (document.querySelector('.minicart .popover').textContent !== '') {
+            console.log(document.querySelector('.minicart .popover .minicart-footer .sub-total').textContent);
+        }        
     });
 };
