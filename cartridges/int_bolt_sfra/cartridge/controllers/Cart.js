d6c9b8273a017d3dbee2ae27809366accd0390b5
Ning W <phpdev2012@126.com>
2023-04-08 20:26:25 +0800

    updates

--- cartridges/int_bolt_sfra/cartridge/controllers/Cart.js
+++ cartridges/int_bolt_sfra/cartridge/controllers/Cart.js
@@ -4,34 +4,34 @@ var server = require('server');
 var page = module.superModule;
 server.extend(page);
 
+const BOLTMINICART = 'minicart';
+const BOLTCART = 'cart';
+
 /* Script Modules */
 var BoltPreferences = require('int_bolt_core/cartridge/scripts/services/utils/preferences');
-var Constants = require('int_bolt_custom/cartridge/scripts/utils/constants');
+var configuration = BoltPreferences.getSitePreferences();
 
 server.append('Show', function (req, res, next) {
-    var configuration = BoltPreferences.getSitePreferences();
     res.setViewData({
         config: configuration,
-        boltVersion: Constants.BOLT_CARTRIDGE_V2_VERSION,
-        isPPC: false
+        isPPC: false,
+        component: BOLTCART
     });
     next();
 });
 
 server.append('MiniCartShow', function (req, res, next) {
-    var configuration = BoltPreferences.getSitePreferences();
     res.setViewData({
         config: configuration,
-        boltVersion: Constants.BOLT_CARTRIDGE_V2_VERSION
+        component: BOLTMINICART
     });
     next();
 });
 
 server.append('MiniCart', function (req, res, next) {
-    var configuration = BoltPreferences.getSitePreferences();
     res.setViewData({
         config: configuration,
-        boltVersion: Constants.BOLT_CARTRIDGE_V2_VERSION
+        component: BOLTMINICART
     });
     next();
 });
@@ -39,13 +39,12 @@ server.append('MiniCart', function (req, res, next) {
 server.get('ReloadBoltButton', function (req, res, next) {
     var renderTemplateHelper = require('*/cartridge/scripts/renderTemplateHelper');
     var buttonResult = {};
-    var configuration = BoltPreferences.getSitePreferences();
     var context = {
         config: configuration,
-        boltVersion: Constants.BOLT_CARTRIDGE_V2_VERSION,
-        isMiniCart: true
+        isMiniCart: true,
+        component: BOLTMINICART
     };
-    buttonResult.html = renderTemplateHelper.getRenderedHtml(context, 'cart/checkoutButtons');
+    buttonResult.html = renderTemplateHelper.getRenderedHtml(context, 'cart/boltButton');
     res.json(buttonResult);
     next();
 });
