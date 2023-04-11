dd0cba0f8b949af0417697e7f40fcaf725dfdd08
Justin Xie <93539162+justinBolt1987@users.noreply.github.com>
2023-04-11 17:24:19 -0400

    Minor bug fixes (#46)

--- cartridges/int_bolt_sfra/cartridge/controllers/Product.js
+++ cartridges/int_bolt_sfra/cartridge/controllers/Product.js
@@ -6,13 +6,11 @@ server.extend(page);
 
 /* Script Modules */
 var BoltPreferences = require('int_bolt_core/cartridge/scripts/services/utils/preferences');
-var Constants = require('int_bolt_custom/cartridge/scripts/utils/constants');
 
 server.append('Show', function (req, res, next) {
     var configuration = BoltPreferences.getSitePreferences();
     res.setViewData({
         config: configuration,
-        boltVersion: Constants.BOLT_CARTRIDGE_V2_VERSION,
         isPPC: true
     });
     next();
