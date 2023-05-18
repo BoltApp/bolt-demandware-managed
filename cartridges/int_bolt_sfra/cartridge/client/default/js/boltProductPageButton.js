21b395bc32f78b0ed64738e7c0356d0238fa6285
var <v.arivazhagan@bolt.com>
2023-05-17 17:12:00 -0700

    Initialize Bolt product button when product is ready

--- cartridges/int_bolt_sfra/cartridge/client/default/js/boltProductPageButton.js
+++ cartridges/int_bolt_sfra/cartridge/client/default/js/boltProductPageButton.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// initProductPageCheckoutButton Polls:
+// showBoltProductPageCheckoutButton Polls:
 // 1) the DOM for the product page checkout button container
 // 2) BoltConnect
 // when they become available, it attaches an onclick event handler to it.
-var initProductPageCheckoutButton = function () {
+var showBoltProductPageCheckoutButton = function () {
     var boltButtonExist = setInterval(function () {
-        var checkoutBoltButton = $('.bolt-product-checkout-button')[0]; // @ts-ignore
+        var checkoutBoltButton = $('.bolt-button-wrapper[data-tid="instant-bolt-checkout-button"]'); // @ts-ignore
         // have to check if child of this dom is svg, otherwise bolt button is not fully rendered (it's the object)
-        var boltButtonLoaded = checkoutBoltButton && window.BoltCheckout;
+        var boltButtonLoaded = checkoutBoltButton && window.BoltCheckout && checkoutBoltButton.children()[0].nodeName === 'svg';
         if (boltButtonLoaded) {
             clearInterval(boltButtonExist);
             console.log('Bolt connect has loaded and product page button is ready!');
@@ -16,7 +16,27 @@ var initProductPageCheckoutButton = function () {
     }, 250);
 };
 
+var callBoltConfigure = function (product) {
+    if (product.available && product.readyToOrder) {
+        console.log('Ready to call BoltCheckout.Configure');
+        console.log(product);
+    } else {
+        console.log('Ready to UnConfigure');
+    }
+};
+
 $(document).ready(function () {
     console.log('Trying to initialize Bolt PPC');
-    initProductPageCheckoutButton();
+
+    // eslint-disable-next-line no-unused-vars
+    $(document).bind('product:updateAddToCart', function (_e, addToCartResponse) {
+        callBoltConfigure(addToCartResponse.product);
+    });
+
+    $(document).bind('product:statusUpdate', function (_e, product) {
+        // This contains the addToCartURL.
+        console.log('On Status Update', product);
+    });
+
+    showBoltProductPageCheckoutButton();
 });
