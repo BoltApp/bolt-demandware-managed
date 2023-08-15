a45265eaeb4e1e808dcc408cbe243f6b39f3df84
justinBolt1987 <jxie@bolt.com>
2023-08-15 18:03:08 -0400

    Remove BOPIS feature from master branch since it's not tested yet.

--- cartridges/int_bolt_bopis/cartridge/models/productLineItem/decorators/fromStoreId.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (object, lineItem) {
-    Object.defineProperty(object, 'fromStoreId', {
-        enumerable: true,
-        value: lineItem.custom.fromStoreId
-    });
-};
--- cartridges/int_bolt_bopis/cartridge/models/productLineItem/decorators/pickUpInStore.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (object, lineItem) {
-    Object.defineProperty(object, 'pickUpInStore', {
-        enumerable: true,
-        value: lineItem.custom.pickUpInStore
-    });
-};
